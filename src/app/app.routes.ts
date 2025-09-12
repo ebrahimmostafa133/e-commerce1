271f4f8a63921335c096be7bb12ea24a62299b0a
Ebrahim Mostafa <ibrahim.mostafa9939@gmail.com>
2025-09-12 06:13:29 +0300

    lazyLoading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,32 +20,106 @@ import { WishlistComponent } from './features/wishlist/wishlist.component';
 import { ProfileComponent } from './features/profile/profile.component';
 
 export const routes: Routes = [
-      {path:'',redirectTo:'home',pathMatch:'full'},
-      {path:'',component:AuthLayoutComponent,
-            canActivate: [isLoggedGuard],
-            children: [
-                  {path:'login',component:LoginComponent,title:'Login'},
-                  {path:'register',component:RegisterComponent,title:'Register'},
-                  {path:'forget',component:ForgetPasswordComponent,title:'ForgetPassword'}
-            ]
-      },
-      {path:'',component:BlankLayoutComponent,
-            canActivate: [authGuard],
-            children: [
-                  {path:'home',component:HomeComponent,title:'Home Page'},
-                  {path:'cart',component:CartComponent,title:'Cart Page'},
-                  {path:'wishlist',component:WishlistComponent,title:'WishList Page'},
-                  {path:'brands',component:BrandsComponent,title:'Brands Page'},
-                  {path:'details/:id/:slug',component:DetailsComponent,title:'Details Page'},
-                  {path:'details/:id',component:DetailsComponent,title:'Details Page'},
-                  {path:'allorders',component:AllOrdersComponent,title:'All Orders Page'},
-                  {path:'products',component:ProductsComponent,title:'Products Page'},
-                  {path:'categories',component:CategoriesComponent,title:'Categories Page'},
-                  {path:'categories/:id', component: CategoryProductsComponent, title: 'Category Products Page' },
-                  {path:'checkout/:id',component:CheckoutComponent,title:'Checkout Page'},
-                  {path:'profile',component:ProfileComponent,title:'Profile Page'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-            ]
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    canActivate: [isLoggedGuard],
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./core/auth/login/login.component').then(m => m.LoginComponent),
+        title: 'Login'
       },
-      {path:'**',component:NotfoundComponent,title:'Not Found'}
+      {
+        path: 'register',
+        loadComponent: () => import('./core/auth/register/register.component').then(m => m.RegisterComponent),
+        title: 'Register'
+      },
+      {
+        path: 'forget',
+        loadComponent: () => import('./core/auth/forget-password/forget-password.component').then(m => m.ForgetPasswordComponent),
+        title: 'ForgetPassword'
+      }
+    ]
+  },
+
+  {
+    path: '',
+    component: BlankLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then(m => m.HomeComponent),
+        title: 'Home Page'
+      },
+      {
+        path: 'cart',
+        loadComponent: () => import('./features/cart/cart.component').then(m => m.CartComponent),
+        title: 'Cart Page'
+      },
+      {
+        path: 'wishlist',
+        loadComponent: () => import('./features/wishlist/wishlist.component').then(m => m.WishlistComponent),
+        title: 'WishList Page'
+      },
+      {
+        path: 'brands',
+        loadComponent: () => import('./features/brands/brands.component').then(m => m.BrandsComponent),
+        title: 'Brands Page'
+      },
+      {
+        path: 'details/:id/:slug',
+        loadComponent: () => import('./features/details/details.component').then(m => m.DetailsComponent),
+        title: 'Details Page',
+        data: { renderMode: 'server' }
+      },
+      {
+        path: 'details/:id',
+        loadComponent: () => import('./features/details/details.component').then(m => m.DetailsComponent),
+        title: 'Details Page',
+        data: { renderMode: 'server' }
+      },
+      {
+        path: 'allorders',
+        loadComponent: () => import('./features/allorders/allorders.component').then(m => m.AllOrdersComponent),
+        title: 'All Orders Page'
+      },
+      {
+        path: 'products',
+        loadComponent: () => import('./features/products/products.component').then(m => m.ProductsComponent),
+        title: 'Products Page'
+      },
+      {
+        path: 'categories',
+        loadComponent: () => import('./features/categories/categories.component').then(m => m.CategoriesComponent),
+        title: 'Categories Page'
+      },
+      {
+        path: 'categories/:id',
+        loadComponent: () => import('./features/categories/categoryproducts/categoryproducts.component').then(m => m.CategoryProductsComponent),
+        title: 'Category Products Page',
+        data: { renderMode: 'server' }
+      },
+      {
+        path: 'checkout/:id',
+        loadComponent: () => import('./features/checkout/checkout.component').then(m => m.CheckoutComponent),
+        title: 'Checkout Page',
+        data: { renderMode: 'server' }
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./features/profile/profile.component').then(m => m.ProfileComponent),
+        title: 'Profile Page'
+      }
+    ]
+  },
+
+  {
+    path: '**',
+    loadComponent: () => import('./features/notfound/notfound.component').then(m => m.NotfoundComponent),
+    title: 'Not Found'
+  }
 ];
